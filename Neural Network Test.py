0b7f1c347f6118ec604ca6265431e99f847cade2
Ching-Han Chou <cbc5577@psu.edu>
2020-07-07 17:38:47 -0400

    Edit Neural Network Test.py

--- Neural Network Test.py
+++ Neural Network Test.py
@@ -12,22 +12,33 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 v = list(training_data)
 # print( list(training_data))
 #
-x = v[0][0]
-y = v[1][0]
+x = np.reshape(v[0][0],(784,))
+y = np.reshape(v[1][0],(784,))
 (t, x1, y1, d1) = (compute_t.get_t(v[0][0], v[1][0], np.identity(784)))
-
-print(sp.euclidean(v[0][0], v[1][0]))
+# result = (compute_t.get_infinity(x, y, np.identity(784)))
+# print(sp.euclidean(v[0][0], v[1][0]))
+# print(result)
 
 net = network.Network([784, 30, 10])
 
-net.SGD(training_data, 2, 10, 3.0, test_data=test_data)
 
+
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+for i in range(30):
+    d = np.load('output {}.npy'.format(i))
+    (t2, x2, y2, d2) = (compute_t.get_t(v[0][0], v[1][0], d))
+    print(t2)
 #print(net.weights)
 
-(t2, x2, y2, d2) = (compute_t.get_t(v[0][0], v[1][0], net.weights[0]))
+
+# result2 = (compute_t.get_infinity(x, y, q))
+# print(result2)
+
 
 from matplotlib import pyplot as plt
 plt.gray()
+plt.imshow(np.reshape(d[0],(28,28)), interpolation='nearest')
+plt.show()
 # plt.imshow(np.reshape(x, (28,28)), interpolation='nearest')
 # plt.show()
 # plt.imshow(np.reshape(x1, (28,28)), interpolation='nearest')
@@ -37,13 +48,13 @@ plt.gray()
 # plt.imshow(np.reshape(y1, (28,28)), interpolation='nearest')
 # plt.show()
 
-plt.imshow(np.reshape(x2, (28,28)), interpolation='nearest')
-plt.show()
-plt.imshow(np.reshape(y2, (28,28)), interpolation='nearest')
-plt.show()
+# plt.imshow(np.reshape(x2, (28,28)), interpolation='nearest')
+# plt.show()
+# plt.imshow(np.reshape(y2, (28,28)), interpolation='nearest')
+# plt.show()
 
 
-plt.imshow(np.reshape(d1, (28,28)), interpolation='nearest')
-plt.show()
-plt.imshow(np.reshape(d2, (28,28)), interpolation='nearest')
-plt.show()
+# plt.imshow(np.reshape(d1, (28,28)), interpolation='nearest')
+# plt.show()
+# plt.imshow(np.reshape(d2, (28,28)), interpolation='nearest')
+# plt.show()
